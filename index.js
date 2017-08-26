dbf7a4c04a1a14bf93f2ec0ae21dfa90e45ef3b9
ra100 <git@ra100.net>
2017-08-26 09:13:29 +0200

    ✨ Add metrics export

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const app = express()
 const client = axios.create({
   timeout: 5000
 })
+prometheus.setNamespace(config.namespace)
+
+const labels = [
+  'name',
+  'id',
+  'license',
+  'type'
+]
+
+prometheus.createGauge('car_lat', 'Latitude position of car', labels)
+prometheus.createGauge('car_lng', 'Longitude position of car', labels)
+prometheus.createGauge('car_km', 'Travel distace with current fuel', labels)
+prometheus.createGauge('car_fuel', 'Fuel level', labels)
+prometheus.createGauge('car_available', 'If car is currently available', labels)
+prometheus.createGauge('car_reservations', 'Current car reservations', labels)
 
 const parseData = (data) => {
   const { cars, car_templates } = data
@@ -18,15 +33,16 @@ const parseData = (data) => {
     const reserved = car.reservations.find(
       reservation => reservation.start < now && reservation.end > now)
     return {
-      car_name: car.name.replace('#', ''),
-      car_id: car.id,
-      car_fuel: car.fuel_level,
-      car_license: car.license,
-      car_position: `${car.lat},${car.lng}`,
-      car_type: `${carTemplate.make} ${carTemplate.model}`,
-      car_km: km,
-      car_available: !reserved || false,
-      car_reservations: car.reservations.length
+      name: car.name.replace('#', ''),
+      id: car.id,
+      license: car.license,
+      type: `${carTemplate.make} ${carTemplate.model}`,
+      lat: car.lat,
+      lng: car.lng,
+      fuel: car.fuel_level,
+      km,
+      available: (!reserved && 1) || 0,
+      reservations: car.reservations.length
     }
   })
   return transformed
@@ -43,13 +59,29 @@ const getData = () =>
       return {}
     })
 
-prometheus.setNamespace(config.namespace)
+const updateMetrics = (data) => {
+  data.forEach((car) => {
+    const carLabels = {
+      name: car.name,
+      id: car.id,
+      license: car.license,
+      type: car.type
+    }
+    prometheus.get('car_lat').set(carLabels, car.lat)
+    prometheus.get('car_lng').set(carLabels, car.lng)
+    prometheus.get('car_km').set(carLabels, car.km)
+    prometheus.get('car_fuel').set(carLabels, car.fuel)
+    prometheus.get('car_available').set(carLabels, car.available)
+    prometheus.get('car_reservations').set(carLabels, car.reservations)
+  })
+}
 
 app.get('/metrics', async (req, res) => {
   const data = await getData()
   const parsed = parseData(data)
-  res.status(200).json(parsed)
-  // res.end(prometheus.getMetrics())
+  updateMetrics(parsed)
+  // res.status(200).json(parsed)
+  res.end(prometheus.getMetrics())
 })
 
 app.listen(config.port, () => {
